80eb1818f481a04b78ffba6ede749c177ecf4154
pomber <pombopombopombo@gmail.com>
2018-08-13 14:08:47 -0300

    Fix missing text tokens

--- src/getHighlightedCodeLines.js
+++ src/getHighlightedCodeLines.js
@@ -1,34 +1,43 @@
 import memoize from "lodash.memoize";
 import Prism from "prismjs";
 
+function parseToken(token, counter) {
+  if (token === "\n") {
+    counter.current = 0;
+    return token;
+  } else if (typeof token === "string" && token.includes("\n")) {
+    const [left, ...rest] = token.split("\n");
+    const right = rest.join("\n");
+    const tokens = addCustomTokens([left, "\n", right], counter);
+    return tokens;
+  } else if (typeof token === "string" && !token.trim()) {
+    // whitespace
+    return token;
+  } else if (typeof token === "string") {
+    counter.current++;
+    return new Prism.Token(
+      "free-text",
+      token,
+      ["token-" + counter.current, "token-leaf"],
+      token
+    );
+  } else if (Prism.util.type(token.content) === "Array") {
+    token.content = addCustomTokens(token.content, counter);
+    return token;
+  } else {
+    counter.current++;
+    const aliases =
+      Prism.util.type(token.alias) === "Array" ? token.alias : [token.alias];
+    aliases.push("token-" + counter.current, "token-leaf");
+    token.alias = aliases;
+    return token;
+  }
+}
+
 function addCustomTokens(tokens, counter = { current: 0 }) {
-  return tokens.map(token => {
-    if (typeof token === "string" && token.includes("\n")) {
-      counter.current = 0;
-      return token;
-    } else if (typeof token === "string" && !token.trim()) {
-      // whitespace
-      return token;
-    } else if (typeof token === "string") {
-      counter.current++;
-      return new Prism.Token(
-        "free-text",
-        token,
-        ["token-" + counter.current, "token-leaf"],
-        token
-      );
-    } else if (Prism.util.type(token.content) === "Array") {
-      token.content = addCustomTokens(token.content, counter);
-      return token;
-    } else {
-      counter.current++;
-      const aliases =
-        Prism.util.type(token.alias) === "Array" ? token.alias : [token.alias];
-      aliases.push("token-" + counter.current, "token-leaf");
-      token.alias = aliases;
-      return token;
-    }
-  });
+  const newTokens = tokens.map(token => parseToken(token, counter));
+  // flatten
+  return newTokens.concat.apply([], newTokens);
 }
 
 function highlight(text, grammar, language) {
@@ -51,7 +60,6 @@ function highlight(text, grammar, language) {
 function getHighlightedCodeLines(code, lang) {
   const html = highlight(code, Prism.languages[lang]);
   const lines = html.split("\n");
-  console.log(lines);
   return lines;
 }
 
